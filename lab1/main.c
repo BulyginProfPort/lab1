f042f40908396d16fbd09be6d9f22b162f67ba81
Fedor <fedya.bulygin@mail.ru>
2022-02-15 12:23:39 +0300

    Initial Commit

---
+++ lab1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  lab1
+//
+//  Created by Фёдор Булыгин on 15.02.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
